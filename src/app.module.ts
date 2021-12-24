e06a059f6a0cef3c475112b235c749f9e540d038
kimbro97 <rlagudwog@gmail.com>
2021-12-24 13:35:18 +0900

    Add posts model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ChatsModule } from './chats/chats.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ChatsModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
