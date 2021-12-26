6271987f99b2fa5908b6a60f589ecf3c1de8a73f
kimbro97 <rlagudwog@gmail.com>
2021-12-26 16:50:42 +0900

    Add import typeorm setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ChatsModule } from './chats/chats.module';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [UsersModule, ChatsModule, PostsModule],
+  imports: [UsersModule, ChatsModule, PostsModule, TypeOrmModule.forRoot(typeORMConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
