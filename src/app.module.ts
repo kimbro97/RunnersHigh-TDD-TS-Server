1ed2e0157673742a282bd387124bff0ba46b6545
kimbro97 <rlagudwog@gmail.com>
2021-12-26 17:13:26 +0900

    Delete typeorm setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
