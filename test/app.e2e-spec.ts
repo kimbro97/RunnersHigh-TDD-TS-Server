7d0ec4ea84f449b769516ac77a6aff92d11730de
kimbro97 <rlagudwog@gmail.com>
2021-12-26 16:49:36 +0900

    Add test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('API 통합 테스트', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,29 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
+  describe('AppModule test', () => {
+    it('/ (GET)', () => {
+      return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
-  });
+      .expect('Hello RunnersHigh!');
+    });
+  })
+
+  describe('UsersModule test', () => {
+    test('/login (POST)', () => {
+      return request(app.getHttpServer())
+      .get('/users/login')
+      .expect(200)
+      .expect('Hello RunnersHigh!');
+    })
+  })
+
+  describe('CahtsModule test', () => {
+    
+  })
+
+  describe('PostsModule test', () => {
+    
+  })
 });
