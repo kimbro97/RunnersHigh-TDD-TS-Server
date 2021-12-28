fc09f453e03a0983cae9f408d1d7d869ab941df9
kimbro97 <rlagudwog@gmail.com>
2021-12-28 23:15:16 +0900

    Add signup api test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,8 +2,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from '../src/configs/typeorm.config';
 
-describe('API 통합 테스트', () => {
+describe('ALL API TEST', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,29 +17,27 @@ describe('API 통합 테스트', () => {
     await app.init();
   });
 
-  describe('AppModule test', () => {
-    it('/ (GET)', () => {
-      return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello RunnersHigh!');
+  
+
+  describe('AppController', () => {
+    it('(GET) /', async () => {
+      const response =  await request(app.getHttpServer()).get('/')
+      expect(response.status).toBe(200)
+      expect(response.text).toBe('Hello RunnersHigh!')
+    });
+  })
+  
+  describe('UsersController', () => {
+    it('(POST) /users/signup', async () => {
+      const fakeUser = {
+        email: 'kimbro@test.com',
+        password: 'hp01300130',
+        nickname: 'kimbro'
+      }
+      const response =  await request(app.getHttpServer()).post('/users/signup').send(fakeUser)
+      expect(response.status).toBe(201)
     });
   })
 
-  describe('UsersModule test', () => {
-    test('/login (POST)', () => {
-      return request(app.getHttpServer())
-      .get('/users/login')
-      .expect(200)
-      .expect('Hello RunnersHigh!');
-    })
-  })
 
-  describe('CahtsModule test', () => {
-    
-  })
-
-  describe('PostsModule test', () => {
-    
-  })
 });
