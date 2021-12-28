5c15f337c4827e023f497bc8fdfcbbfee8b99823
kimbro97 <rlagudwog@gmail.com>
2021-12-28 23:14:07 +0900

    Add users module

---
+++ src/users/users.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UserRepository } from '../repository/user.repository';
+
+@Injectable()
+export class UsersService {
+    constructor(@InjectRepository(UserRepository) private userRepository: UserRepository) {}
+
+    createUser(createUserDto: CreateUserDto) {
+      return this.userRepository.createUser(createUserDto)
+    }
+}
