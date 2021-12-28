c790d349e0962be1ea5080f3712a8d5341442c86
kimbro97 <rlagudwog@gmail.com>
2021-12-28 23:16:08 +0900

    Add nestjs setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ChatsModule } from './chats/chats.module';
+import { PostsModule } from './posts/posts.module';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(typeORMConfig),
+    UsersModule,
+    ChatsModule,
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
