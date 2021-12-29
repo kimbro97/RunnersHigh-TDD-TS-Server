bbc0c25c982c27edf30212d2929785489703dde2
kimbro97 <rlagudwog@gmail.com>
2021-12-29 23:02:45 +0900

    Add post /users/login api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { User } from '../entitys/user.entity';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UsersService } from './users.service';
+import { LoginDto } from '../dto/login-user.dto';
 
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService){}
+
     @Post('/signup')
-    createUser(@Body() createUserDto: CreateUserDto) {
+    createUser(@Body() createUserDto: CreateUserDto): Promise<void> {
         return this.usersService.createUser(createUserDto)
+    }
+
+    @Post('/login')
+    logIn(@Body() loginDto: LoginDto): Promise<User> {
+        return this.usersService.loginUser(loginDto)
     }
 }
