64860515690ef87d3aea4c242fa9a09dd6be66c7
kimbro97 <rlagudwog@gmail.com>
2021-12-29 23:03:17 +0900

    Add post /users/login api

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../entitys/user.entity';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UserRepository } from '../repository/user.repository';
+import { LoginDto } from '../dto/login-user.dto';
 
 @Injectable()
 export class UsersService {
     constructor(@InjectRepository(UserRepository) private userRepository: UserRepository) {}
 
-    createUser(createUserDto: CreateUserDto) {
+     async createUser(createUserDto: CreateUserDto): Promise<void> {
       return this.userRepository.createUser(createUserDto)
+    }
+
+    async loginUser(loginDto: LoginDto): Promise<User> {
+      return this.userRepository.loginUser(loginDto)
     }
 }
