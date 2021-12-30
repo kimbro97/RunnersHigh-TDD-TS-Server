cb391aec44b6397a458f7fb6944ed887a2d7b0f6
kimbro97 <rlagudwog@gmail.com>
2021-12-30 17:19:16 +0900

    Add jwt token interface

---
+++ src/dto/jwt.dto.ts
@@ -0,0 +1,9 @@
+export interface TokenDto {
+    id:number;
+    email: string;
+    nickname: string;
+    image_url: string;
+    social_type: string;
+    createdAt: string;
+    updatedAt: string;
+}
