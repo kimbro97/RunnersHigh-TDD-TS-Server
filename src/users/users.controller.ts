5807caaed9d62c238fd530ef2f589444593aefd8
kimbro97 <rlagudwog@gmail.com>
2021-12-30 17:16:45 +0900

    Add post /users/signout api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,8 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, Headers, Post } from '@nestjs/common';
 import { User } from '../entitys/user.entity';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UsersService } from './users.service';
 import { LoginDto } from '../dto/login-user.dto';
-
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService){}
@@ -12,9 +11,18 @@ export class UsersController {
     createUser(@Body() createUserDto: CreateUserDto): Promise<void> {
         return this.usersService.createUser(createUserDto)
     }
-
+    
+    @Post('/signout')
+    deleteUser(@Headers('authorization') authorization: String) {
+        return this.usersService.deleteUser(authorization)
+    }
     @Post('/login')
     logIn(@Body() loginDto: LoginDto): Promise<User> {
         return this.usersService.loginUser(loginDto)
+    }
+
+    @Post('/logout')
+    logout(@Headers('authorization') authorization: string){
+        return this.usersService.logoutUser(authorization)
     }
 }
