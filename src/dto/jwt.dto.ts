e6097106985730f140d32400d070d8a7befd9503
kimbro97 <rlagudwog@gmail.com>
2021-12-30 20:36:34 +0900

    Delete TokenDto

--- src/dto/jwt.dto.ts
+++
@@ -1,9 +0,0 @@
-export interface TokenDto {
-    id:number;
-    email: string;
-    nickname: string;
-    image_url: string;
-    social_type: string;
-    createdAt: string;
-    updatedAt: string;
-}
