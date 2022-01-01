f18f8bc53ce8a8d78db6d8cfb2fc82e016f1fbe2
kimbro97 <rlagudwog@gmail.com>
2022-01-01 21:37:07 +0900

    Add patch editUserInfo router

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,21 @@
-import { Body, Controller, Headers, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Headers, Patch, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { User } from '../entitys/user.entity';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UsersService } from './users.service';
 import { LoginDto } from '../dto/login-user.dto';
+import { FileInterceptor} from '@nestjs/platform-express';
+import { EditUserInfoDto } from '../dto/edit-user.dto';
+
+
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService){}
-
     @Post('/signup')
     createUser(@Body() createUserDto: CreateUserDto): Promise<void> {
         return this.usersService.createUser(createUserDto)
     }
     
-    @Post('/signout')
+    @Delete('/userinfo')
     deleteUser(@Headers('authorization') authorization: String) {
         return this.usersService.deleteUser(authorization)
     }
@@ -22,7 +25,18 @@ export class UsersController {
     }
 
     @Post('/logout')
-    logout(@Headers('authorization') authorization: string){
+    logout(@Headers('authorization') authorization: string): Promise<void> {
         return this.usersService.logoutUser(authorization)
+    }
+
+    @Get('/userinfo')
+    getUserInfo(@Headers('authorization') authorization: string): Promise<void> {
+        return this.usersService.getUserInfo(authorization)
+    }
+
+    @Patch('/userinfo')
+    @UseInterceptors(FileInterceptor('userimage'))
+    editUserInfo(@Headers('authorization') authorization: string, @UploadedFile() file: Express.Multer.File, @Body() editUserDto: EditUserInfoDto) {
+        return this.usersService.editUserInfo(authorization,file, editUserDto)
     }
 }
