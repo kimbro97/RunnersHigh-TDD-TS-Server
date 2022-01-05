319c4ddb85f85194d0d3feec4a00c7a92c2caae8
kimbro97 <rlagudwog@gmail.com>
2022-01-05 22:56:00 +0900

    Add Post entity User entity relations

---
+++ src/entitys/post.entity.ts
@@ -0,0 +1,29 @@
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "./user.entity";
+
+@Entity()
+export class Post extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    text: string;
+
+    @Column()
+    thumbnail_url: string;
+
+    @Column()
+    location: string;
+
+    @Column()
+    latitude: string;
+
+    @Column()
+    longitude: string;
+
+    @ManyToOne(type => User, user => user.id, { eager: false })
+    userId: User;
+}
